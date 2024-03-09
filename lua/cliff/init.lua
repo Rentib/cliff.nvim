89e62a590595dce8a98ee96a030d451325567d77
Rentib <sbitner420@tutanota.com>
2024-03-09 20:55:22 +0100

    fix: virtcol and col 0
    Apparently API does not support virtual text. This caused some weird
    behaviour. Also there was wrong inequality in second while loop, which
    is fixed now. It should move the cursor at the beginning of the line in
    more natural way.

--- lua/cliff/init.lua
+++ lua/cliff/init.lua
@@ -1,8 +1,7 @@
 local move = function(key)
     -- NOTE: use norm command because nvim_win_set_cursor doesnt support virtual edit
 
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local row, col = cursor[1], cursor[2]
+    local row, col = vim.fn.line('.'), vim.fn.virtcol('.')
 
     local first_lnum = vim.fn.line('0')
     local last_lnum = vim.fn.line('$')
@@ -26,7 +25,7 @@ local move = function(key)
 
     while lnum >= first_lnum and lnum <= last_lnum + 1 do
         cnt = cnt + 1
-        if #vim.fn.getline(lnum) > col then
+        if #vim.fn.getline(lnum) >= col then
             break
         end
         lnum = lnum + diff
