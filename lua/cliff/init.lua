19129552be1b1b8e1b3b5725e565e2a30226100b
Rentib <sbitner420@tutanota.com>
2024-03-12 00:28:40 +0100

    fix: fixed behaviour with noexpandtab
    For files with tabs used as indentation the behaviour was very weird as
    the tabs were counted as a single character. This commit changes this
    and counts leading tabs as `shiftwidth` characters in order to make the
    behaviour more similar to that when using expandtab.
    
    The plugin still does not support wide chars and tabs used in the middle
    of a line.

--- lua/cliff/init.lua
+++ lua/cliff/init.lua
@@ -1,3 +1,27 @@
+local lineLength = function(lnum)
+    local tab_hex_code = 9
+
+    -- TODO: tabsize might be different than shiftwidth
+    local expandtab = vim.api.nvim_buf_get_option(0, "expandtab")
+    local tabsize   = vim.api.nvim_buf_get_option(0, "shiftwidth")
+    local line      = vim.fn.getline(lnum)
+    local length    = #line
+
+    if expandtab then
+        return length
+    end
+
+    -- count tabs used as indentation
+    for i = 1, #line do
+        if line:byte(i) ~= tab_hex_code then
+            break
+        end
+        length = length + tabsize - 1
+    end
+
+    return length
+end
+
 local move = function(key)
     -- NOTE: use norm command because nvim_win_set_cursor doesnt support virtual edit
 
@@ -12,9 +36,7 @@ local move = function(key)
 
     while lnum >= first_lnum and lnum <= last_lnum + 1 do
         cnt = cnt + 1
-        if #vim.fn.getline(lnum) < col then
-            break
-        end
+        if lineLength(lnum) < col then break end
         lnum = lnum + diff
     end
 
@@ -25,9 +47,7 @@ local move = function(key)
 
     while lnum >= first_lnum and lnum <= last_lnum + 1 do
         cnt = cnt + 1
-        if #vim.fn.getline(lnum) >= col then
-            break
-        end
+        if lineLength(lnum) >= col then break end
         lnum = lnum + diff
     end
 
